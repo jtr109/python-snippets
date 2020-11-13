0e30758be7fadbda84bc7b8710e1480a49c581f8
jtr109 <conbas2019@gmail.com>
2020-11-13 11:17:31 +0800

    feat: add comment

--- django-async-demo/project/views.py
+++ django-async-demo/project/views.py
@@ -2,6 +2,7 @@ import asyncio
 import random
 from typing import List
 
+import aiohttp
 from asgiref.sync import async_to_sync
 from django.http.response import HttpResponse
 
@@ -9,9 +10,11 @@ from django.http.response import HttpResponse
 async def request_one(url: str) -> str:
     seconds = random.randint(1, 2)
     print(f'request {url}')
-    await asyncio.sleep(seconds)  # request a url
-    print(f'get response from {url}')
-    return ''.join(list(reversed(url)))
+    # asyncio 中不能使用 requests 模块，由于 requests 模块是同步的，请求还是会同步执行。
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            print(f'get response from {url}')
+    return url
 
 
 @async_to_sync
@@ -22,7 +25,18 @@ async def request_all(urls: List[str]):
     print(results)
 
 
+def run(urls: List[str]):
+    # async_to_sync 的转换可以放在调用栈的任何深度
+    request_all(urls)
+
+
 # https://docs.djangoproject.com/en/3.1/topics/async/
 def hello(request):
-    request_all(urls=['foo', 'bar'])
+    urls = [
+        'https://www.github.com',
+        'https://www.baidu.com',
+        'https://www.zhihu.com',
+        'https://www.godaddy.com',
+    ]
+    run(urls)
     return HttpResponse('hello')
